e9e675935066ae6569d7e17cc57e6f72ac9a8c4a
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-21 11:28:01 -0500

    Work on toggle for color change and text size

--- options.js
+++ options.js
@@ -1,83 +1,43 @@
-var chattyCluck = (function(){
+console.log("Welcome to the Jungle!")
+
+// // var chattyCluck = (function(){
 	
-}(chattyCluck || {}));
+// // }(chattyCluck || {}));
 
-
-console.log("Ahoy, Captain!");
-
-let outputDiv = document.getElementById("output");
-
-let articleEl = document.getElementsByClassName("article-section");
-// console.log("articleEl");
-
+let firstOptionOutput = document.getElementsByClassName("checkboxOne");
+let secondOptionOutput = document.getElementsByClassName("checkboxTwo");
+console.log(secondOptionOutput);
 
 function handleSectionClick() {
 	console.log(event)
 	let elementText = event.target.innerHTML
-	outputDiv.innerHTML = `<p class="msg">You clicked on the ${elementText} section</p>`;
+	firstOptionOutput.innerHTML =  `<p class="msg">You clicked on the ${elementText} section</p>`;
 };
 
-
-// handleSectionClick();
-
-articleEl[0].addEventListener("click", handleSectionClick);
-
+secondOptionOutput[0].addEventListener("click", handleSectionClick);
 
 function sayYourName(name) {
 	console.log("My name is " + name );
-}
+};
+
+let piggie = document.getElementsByClassName("checkboxOne", "checkboxTwo");
 
 
-let piggie = document.getElementById("guinea-pig");
-
-
-document.getElementById("add-color").addEventListener("click", function() {
-	piggie.classList.toggle("blue");
-	console.log(piggie, piggie.classList)
+document.getElementById("checkboxOne").addEventListener("click", function() {
+	piggie.classList.toggle("neonDream");
+	console.log(piggie, piggie.classList);
 });
 
-
-document.getElementById("make-large").addEventListener("click", function() {
-	piggie.classList.toggle("hulk");
+document.getElementById("checkboxTwo").addEventListener("click", function() {
+	piggie.classList.toggle("hulkify");
 	console.log(piggie, piggie.classList)
 });
-
-
-document.getElementById("add-border").addEventListener("click", function() {
-	piggie.classList.toggle("borderPatrol");
-	console.log(piggie, piggie.classList)
-});
-
-
-document.getElementById("add-rounding").addEventListener("click", function() {
-	piggie.classList.toggle("rounding");
-	console.log(piggie, piggie.classList)
-});
-
-
 
 // ***********************************
 // Where you click (the event) vs where the listener is attached
-
 
 document.getElementById("pig-wrapper").addEventListener("click", function() {
 	console.log("WHAT I clicked on", event.target);
 	console.log("WHERE the listener is attached", event.currentTarget);
 });
-
-
-
-
-
-
-
-console.log("Still going!"); 
-
-
-
-
-
-
-
-
 

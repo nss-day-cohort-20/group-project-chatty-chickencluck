7f952441d7978bd260ac3b90de7a4a5abb921a1c
Glen Vollmer <glenvollmer@Glens-Macbook-Pro.local>
2017-06-21 09:44:35 -0500

    added the getElements in the main.js file and fine tuned the html

---
+++ main.js
@@ -0,0 +1,11 @@
+let msgInput = document.getElementById("msgInput");
+
+let clearBtn = document.getElementById("clearBtn");
+
+let checkboxOne = document.getElementsByClassName("checkboxOne");
+
+let checkboxTwo = document.getElementsByClassName("checkboxTwo");
+
+let msgBoard = document.getElementsByClassName("msgBoard");
+
+// event listeners will eventually need to be added

8707435b6599a5d1de3cebe520bc0f3b916fd32a
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-22 12:59:55 -0500

    continue to clean up main JS

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
+// creates input space for user
 let msgInput = document.getElementById("msgInput");
+
+// creates clear button for message box
 let clearBtn = document.getElementById("clearBtn");
+
+// dark theme checkbox
 let checkboxOne = document.getElementsByClassName("checkboxOne");
+
+// large text checkbox
 let checkboxTwo = document.getElementsByClassName("checkboxTwo");
+
+// creates acces to message board
 let msgBoard = document.getElementById("msgBoard");
 
+// creates an object for dark theme
 let firstOption = document.getElementById("checkboxOne");
+
+// creates an object for large text 
 let secondOption = document.getElementById("checkboxTwo");
 
 firstOption.addEventListener("click", function () {

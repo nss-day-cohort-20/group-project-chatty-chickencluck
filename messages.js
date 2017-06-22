34158c11a47e4c3da80e8fda4c1ad9156069f52a
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-06-22 09:15:26 -0500

    seperated json and messages

--- messages.js
+++ messages.js
@@ -1,9 +1,8 @@
 //global var
 var chattyCluck = (function(chattyCluckObj){
   
-chattyCluckObj.name = "fred"
-msgInput.value
-let emptyMsg = [];
+  // msgInput.value
+  let emptyMsg = [];
         
   chattyCluckObj.makeArr = function() {
     emptyMsg.push(`<p>${msgInput.value}</p>`)
@@ -17,8 +16,8 @@ let emptyMsg = [];
   msgInput.addEventListener("keyup", function(event) {
       event.preventDefault();
       if (event.keyCode == 13) {
-        makeArr()
-        clearText()
+        chattyCluckObj.makeArr()
+        chattyCluckObj.clearText()
       msgBoard.innerHTML = emptyMsg.join("");
       }
   });
@@ -26,44 +25,3 @@ let emptyMsg = [];
   return chattyCluckObj
 
 }(chattyCluck || {}));
-
-
-//------------------------------------------------------------
-
-let messages = null;
-
-
-
-let newMessages = document.getElementById("msgBoard");
-function outputMessages(premadeMessages) {
-  console.log("premadeMessages", premadeMessages);
-  for(var i = 0; i < premadeMessages.length; i++) {
-    let msgBoard = document.createElement("article");
-    let msgText = document.createElement("p");
-    msgText.innerHTML = premadeMessages[i].text;
-    msgBoard.innerHTML = premadeMessages[i].name;
-    newMessages.appendChild(msgBoard);
-    newMessages.appendChild(msgText);
-  }
-}
-
-
-
-
-function setMessages() {
-  messages = JSON.parse(event.target.responseText).messages;
-  console.log("messages", messages);
-  outputMessages(messages)
-}
-
-
-
-function getMessages() {
-  let reqMessages = new XMLHttpRequest();
-  reqMessages.addEventListener("load", setMessages);
-  reqMessages.open("GET", "messages.json");
-  reqMessages.send();
-}
-getMessages();
-
-//TODO look into forEach, create element, append child

e56ec37f369c73c9eda35693155d49ff40592cf5
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-22 14:09:13 -0500

    work on linking a getter in default to messages JS

--- defaultmsgs.js
+++ defaultmsgs.js
@@ -1,23 +1,23 @@
 var chattyCluck = function(loadJson){
   let messages = null;
 
-  let newMessages = document.getElementById("msgBoard");
-  function outputMessages(premadeMessages) {
-    console.log("premadeMessages", premadeMessages);
-    for(var i = 0; i < premadeMessages.length; i++) {
-      let msgBoard = document.createElement("article");
-      let msgText = document.createElement("p");
-      msgText.innerHTML = premadeMessages[i].text;
-      msgBoard.innerHTML = premadeMessages[i].name;
-      newMessages.appendChild(msgBoard);
-      newMessages.appendChild(msgText);
-    }
-  }
+  // let newMessages = document.getElementById("msgBoard");
+  // function outputMessages(premadeMessages) {
+  //   console.log("premadeMessages", premadeMessages);
+    // for(var i = 0; i < premadeMessages.length; i++) {
+      // let msgBoard = document.createElement("article");
+      // let msgText = document.createElement("p");
+      // msgText.innerHTML = premadeMessages[i].text;
+      // msgBoard.innerHTML = premadeMessages[i].name;
+      // newMessages.appendChild(msgBoard);
+      // newMessages.appendChild(msgText);
+  //   }
+  // }
 
   function setMessages() {
     messages = JSON.parse(event.target.responseText).messages;
     console.log("messages", messages);
-    outputMessages(messages)
+    // outputMessages(messages)
   }
 
   function loadMessages() {
@@ -28,6 +28,7 @@ var chattyCluck = function(loadJson){
   }
 
   loadJson.getMessages = function() {
+    console.log(messages);
     return messages
   }
 

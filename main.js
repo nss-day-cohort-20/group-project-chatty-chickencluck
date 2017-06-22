ed5728336b515ecfe7224466830c56e1efc64793
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-22 11:41:13 -0500

    moveevent listeners and toggle from options JS into main JS

--- main.js
+++ main.js
@@ -4,5 +4,17 @@ let checkboxOne = document.getElementsByClassName("checkboxOne");
 let checkboxTwo = document.getElementsByClassName("checkboxTwo");
 let msgBoard = document.getElementById("msgBoard");
 
+let firstOption = document.getElementById("checkboxOne");
+let secondOption = document.getElementById("checkboxTwo");
+
+firstOption.addEventListener("click", function () {
+	document.body.classList.toggle("darkTheme");
+	console.log("You just changed the color!")
+	});
+	
+secondOption.addEventListener("click", function () {
+	document.body.classList.toggle("largeText");
+	console.log("You just changed the size!")
+	});
 
 // think of a way to use the '.find' to incorporate finding properties of an object to delete
